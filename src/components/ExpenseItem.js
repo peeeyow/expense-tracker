5ee99946ded332b677d69603c6fae8ad100de52c
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-05-29 15:01:03 +0800

    Layout ExpenseItem card

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,13 @@
+import "./ExpenseItem.css";
+
 export default function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+  return (
+    <div className="expense-item">
+      <div>May 29, 2022</div>
+      <div className="expense-item__description">
+        <h2>Keyboard</h2>
+        <div className="expense-item__price">100.00</div>
+      </div>
+    </div>
+  );
 }
