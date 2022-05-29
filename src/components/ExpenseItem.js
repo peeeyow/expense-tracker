ebbb06b59123ed0788fbc5b13f904b78871766da
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-05-29 16:16:02 +0800

    Change hardcoded values to props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>May 29, 2022</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Keyboard</h2>
-        <div className="expense-item__price">100.00</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.price}</div>
       </div>
     </div>
   );
