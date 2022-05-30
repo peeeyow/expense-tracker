519630a88526034c4bb6b5ff048e403de7681d7b
Pio Mendoza <pio.lauren.mendoza@gmail.com>
2022-05-30 21:41:56 +0800

    Add NewExpense component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -25,7 +26,7 @@ function App() {
   ];
   return (
     <div className="App">
-      <h1>"Test File"</h1>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
